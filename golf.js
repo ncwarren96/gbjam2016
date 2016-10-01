1a973c0f7cb565f31440793b23907f544e27e526
Nicholas Warren <ncwarren@ucsc.edu>
2016-10-01 15:16:12 -0700

    getting started
    ball on screen and game loop

--- golf.js
+++ golf.js
@@ -1,5 +1,28 @@
 //Canvas stuff
-var canvas = $("#canvas")[0];
-var ctx = canvas.getContext("2d");
-var width = $("#canvas").width();
-var height = $("#canvas").height();
+	var canvas = document.getElementsByTagName('canvas')[0];
+	var ctx = canvas.getContext("2d");
+	ctx.imageSmoothingEnabled = false;
+//var width = $("#canvas").width();
+//var height = $("#canvas").height();
+
+function init(){
+
+	console.log("init");
+	
+	//Test ball
+	var stage = new createjs.Stage("canvas");
+	var ball = new createjs.Shape();
+	ball.graphics.beginFill("green").drawCircle(0, 0, 2);
+	ball.x = 10;
+	ball.y = 10;
+	stage.addChild(circle);
+	
+	//Ticker
+	createjs.Ticker.on("tick", game_loop);
+	createjs.Ticker.setFPS(30);
+	
+}
+function game_loop(event) {
+	update();
+	draw();
+}
