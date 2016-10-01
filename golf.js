62456a050bfc4fabc29c9e18dddfff7b94871316
Nicholas Warren <ncwarren@ucsc.edu>
2016-10-01 16:08:16 -0700

    starting some physics

--- golf.js
+++ golf.js
@@ -5,18 +5,25 @@
 //var width = $("#canvas").width();
 //var height = $("#canvas").height();
 
+
+var stage;
+var ball, speedX, speedY, power; 
 function init(){
 
 	console.log("init");
+	stage = new createjs.Stage("canvas");
 	
 	//Test ball
-	var stage = new createjs.Stage("canvas");
-	var ball = new createjs.Shape();
+	ball = new createjs.Shape();
 	ball.graphics.beginFill("green").drawCircle(0, 0, 2);
 	ball.x = 10;
 	ball.y = 10;
-	stage.addChild(circle);
+	stage.addChild(ball);
 	
+	speedX = 3;
+	speedY = 1;
+	power = 0;
+		
 	//Ticker
 	createjs.Ticker.on("tick", game_loop);
 	createjs.Ticker.setFPS(30);
@@ -26,3 +33,37 @@ function game_loop(event) {
 	update();
 	draw();
 }
+function update(){
+	ball.x += speedX;
+	speedX = speedX*.95;
+	stage.update(event);
+	
+	console.log(speedX);
+	var inc = true;
+	
+	if(power>100){
+		inc = !inc;
+	}
+	
+	if(inc){
+		power++;
+	}else{
+		power--;
+	}
+}
+
+ function draw(){
+ 	
+}
+
+
+
+
+
+
+
+
+
+
+
+
